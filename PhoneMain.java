87a656a4fb4279128d8bfb1483a2ffc4f82d335f
seona-dev <94502314+seona-dev@users.noreply.github.com>
2021-11-26 13:01:15 +0900

    Add files via upload

--- PhoneMain.java
+++ PhoneMain.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
+class NumberException extends Exception
+{
+	int number;
+
+	NumberException(int number)
+	{
+		this.number = number;
+	}
+	public String getMessage()
+	{
+		return number+"에 해당하는 선택은 존재하지 않습니다.";
+	}
+}
+
 interface Menu
 {
 	int INPUT_INFO = 1;
@@ -24,57 +38,66 @@ class PhoneMain implements Menu
 
 		while(true)
 		{
-			PhoneUI.selectUI();
-			int select = sc.nextInt();
-			sc.nextLine();
-			System.out.println();
-
-			switch(select)
-			{
-			case INPUT_INFO :
-				PhoneUI.categoryUI();
-				int category = sc.nextInt();
+			try{
+				PhoneUI.selectUI();
+				int select = sc.nextInt();
 				sc.nextLine();
 				System.out.println();
-				switch(category)
+
+				switch(select)
 				{
-				case GENERAL :
-					PhoneUI.generalInputUI();
+				case INPUT_INFO :
+					PhoneUI.categoryUI();
+					int category = sc.nextInt();
+					sc.nextLine();
+					System.out.println();
+					switch(category)
+					{
+					case GENERAL:
+						PhoneUI.generalInputUI();
+						break;
+					case UNIV:
+						PhoneUI.univInputUI();
+						break;
+					case COMPANY:
+						PhoneUI.companyInputUI();
+						break;
+					default:
+						throw new NumberException(category);
+					}
 					break;
-				case UNIV :
-					PhoneUI.univInputUI();
+				case SEARCH_INFO : 
+					PhoneUI.searchUI();
 					break;
-				case COMPANY :
-					PhoneUI.companyInputUI();
+				case DELETE_INFO :
+					PhoneUI.deleteUI();
 					break;
+				case SHOW_ALL :
+					PhoneUI.showAllUI();
+					break;
+				case EXIT_PROGRAM :
+					PhoneUI.exitUI();
+					int exit = sc.nextInt();
+					sc.nextLine();
+					switch(exit)
+					{
+					case YES : 
+						System.out.println("프로그램을 종료합니다.");
+						sc.close();
+						return;
+					case NO : 
+						System.out.println("메뉴 화면으로 돌아갑니다.");
+						break;
+					default :
+						throw new NumberException(exit);
+					}
+					break;
+				default:
+					throw new NumberException(select);
 				}
-				break;
-			case SEARCH_INFO : 
-				PhoneUI.searchUI();
-				break;
-			case DELETE_INFO :
-				PhoneUI.deleteUI();
-				break;
-			case SHOW_ALL :
-				PhoneUI.showAllUI();
-				break;
-			case EXIT_PROGRAM :
-				PhoneUI.exitUI();
-				int exit = sc.nextInt();
-				sc.nextLine();
-				switch(exit)
-				{
-				case YES : 
-					System.out.println("프로그램을 종료합니다.");
-					sc.close();
-					return;
-				case NO : 
-					System.out.println("메뉴 화면으로 돌아갑니다.");
-					break;
-				}
-				break;
-			default:
-				PhoneUI.wrongUI();
+			} catch (NumberException e) {
+				System.out.println(e.getMessage());
+				System.out.println("메뉴 선택을 처음부터 다시 진행합니다.");
 			}
 		}
 	}
